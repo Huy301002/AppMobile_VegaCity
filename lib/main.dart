cd9f7288d152d9d3643954876a6319f7f6a9fd75
huy301002 <huyhoangrt009@gmail.com>
2024-08-18 17:42:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/View/splash_screen.dart';
+import 'package:flutter_application_1/View/welcome_screen.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      // title: 'POS',
+      // initialRoute: '/',
+      // getPages: [
+      // GetPage(
+      //   name: '/',
+      //   page: () => SplashScreen(),
+      //   transition: Transition.fadeIn,
+      // ),
+      // GetPage(
+      //   name: '/welcome',
+      //   page: () => WelcomeScreen(),
+      //   transition: Transition.zoom,
+      // ),
+      // GetPage(
+      //   name: '/home',
+      //   page: () => const HomeScreen(),
+      //   transition: Transition.rightToLeft,
+      // ),
+      // ],
+      home: WelcomeScreen(),
+    );
+  }
+}
