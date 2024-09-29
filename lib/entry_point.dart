cbea20d5984bffbf36a71f77aea88fbdf78ca8c5
huy301002 <huyhoangrt009@gmail.com>
2024-09-30 02:32:57 +0700

    update

--- lib/entry_point.dart
+++ lib/entry_point.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_application_1/View/signup/login_screen.dart';
-import 'View/profile/profile_screen.dart';
-import 'View/scanner_QR/scanner_screen.dart';
+// import 'View/profile/profile_screen.dart';
+// import 'View/scanner_QR/scanner_screen.dart';
 import 'package:flutter_application_1/View/history/history_screen.dart';
 import 'package:flutter_application_1/View/home/Home_Screen.dart';
 import 'package:flutter_application_1/View/package_screen.dart';
@@ -24,9 +24,9 @@ class _EntryPointState extends State<EntryPoint> {
   static final List<Widget> _screens = [
     HomeScreen(),
     PackageScreen(),
-    ScannerScreen(),
+    // ScannerScreen(),
     HistoryScreen(),
-    ProfileScreen(),
+    // ProfileScreen(),
   ];
 
   @override
